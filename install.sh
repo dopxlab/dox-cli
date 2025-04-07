6a0c6a866d3f9b95b64e190cb7c55d3bbd24597a
Joby Pooppillikudiyil <joby81@gmail.com>
2025-04-07 18:18:40 +0200

    dox dir removing .

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 
 set -e
 
+export DOX_DIR="$HOME/lib/dox"
+
 # Define the URL for the latest release tarball and the install directory
 DOX_RELEASE_URL="https://github.com/dopxlab/dox-cli/releases/latest/download/dox-cli.tar.gz"
-INSTALL_DIR="$HOME/.dox"
 
-mkdir -p "$INSTALL_DIR"
+mkdir -p "$DOX_DIR"
 
 # Download, extract, and set up DOX CLI
-curl -LO $DOX_RELEASE_URL && tar -xzf dox-cli.tar.gz -C "$INSTALL_DIR" && rm -f dox-cli.tar.gz
+curl -LO $DOX_RELEASE_URL && tar -xzf dox-cli.tar.gz -C "$DOX_DIR" && rm -f dox-cli.tar.gz
 
 # Add the bin directory to PATH if it's not already there
-grep -q "$INSTALL_DIR/bin" "$HOME/.bashrc" || echo "export PATH=\"$INSTALL_DIR/bin:\$PATH\"" >> "$HOME/.bashrc" && source "$HOME/.bashrc"
+grep -q "$DOX_DIR/bin" "$HOME/.bashrc" || echo "export PATH=\"$DOX_DIR/bin:\$PATH\"" >> "$HOME/.bashrc" && source "$HOME/.bashrc"
 
 echo "✅ DOX CLI installed successfully!"
