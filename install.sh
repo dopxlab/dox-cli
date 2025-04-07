73cb7b1f024044a3bffbb0115fc6fc6a1b7ee8f7
Joby Pooppillikudiyil <joby81@gmail.com>
2025-04-07 18:03:15 +0200

    including create dirs

--- install.sh
+++ install.sh
@@ -5,12 +5,13 @@ set -e
 # Define the URL for the latest release tarball and the install directory
 DOX_RELEASE_URL="https://github.com/dopxlab/dox-cli/releases/latest/download/dox-cli.tar.gz"
 INSTALL_DIR="$HOME/lib/dox"
-BIN_DIR="$INSTALL_DIR/bin"
+
+mkdir -p "$INSTALL_DIR"
 
 # Download, extract, and set up DOX CLI
 curl -LO $DOX_RELEASE_URL && tar -xzf dox-cli.tar.gz -C "$INSTALL_DIR" && rm -f dox-cli.tar.gz
 
 # Add the bin directory to PATH if it's not already there
-grep -q "$BIN_DIR" "$HOME/.bashrc" || echo "export PATH=\"$INSTALL_DIR/bin:\$PATH\"" >> "$HOME/.bashrc" && source "$HOME/.bashrc"
+grep -q "$INSTALL_DIR/bin" "$HOME/.bashrc" || echo "export PATH=\"$INSTALL_DIR/bin:\$PATH\"" >> "$HOME/.bashrc" && source "$HOME/.bashrc"
 
 echo "✅ DOX CLI installed successfully!"
