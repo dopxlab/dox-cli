22f0d33a5f6dd6c1367e9071b42d9add17a73d9c
Joby Pooppillikudiyil <joby81@gmail.com>
2025-04-13 20:30:08 +0200

    handling env variables - cleanup 2

--- lib/configure.sh
+++ lib/configure.sh
@@ -11,12 +11,11 @@ source ${DOX_CUSTOM_DIR}/download_files.sh
 # configuration yaml location
 CONFIGURE_FILE_PATH="${DOX_CUSTOM_DIR}/configure"
 ENV_PATH="env_path"
-ENV_EXPORT="env_export"
 
-print_envs DOX_RESOURCES_DIR CONFIGURE_FILE_PATH DOX_DIR DOX_CUSTOM_DIR ENV_PATH ENV_EXPORT
+print_envs DOX_RESOURCES_DIR CONFIGURE_FILE_PATH DOX_DIR DOX_CUSTOM_DIR
 
 # Function to set environment variables for a given library
-function generate_env_files() {
+function configure_env_variables() {
     local lib=$1
     local version=$2
     local install_dir=$3
@@ -38,48 +37,28 @@ function generate_env_files() {
             local evaluated_value=$(eval echo "$value")
             # Print the evaluated value
             #echo "Evaluated Value: $evaluated_value"
-            if [ "$key" == "PATH" ]; then # If key is PATH, save it to ENV_PATH
-                append_if_not_exists $ENV_PATH "$evaluated_value:"
+            if [ "$key" == "PATH" ]; then # If key is PATH, save it to PATH
+                append_if_not_exists "$evaluated_value:"
             else # For other keys, save them to the env.sh script
-                append_if_not_exists $ENV_EXPORT "export $key=\"$evaluated_value\";"
+                export "$key=$evaluated_value"
             fi
         done
     fi
-    configure_env_variables # used for post_installation_scripts
 }
 
 # Function to append a value to a file if it doesn't already exist
 function append_if_not_exists() {
-    local file_path=$1
-    local value_to_append=$2
-    # Check if the value already exists in the file
-    if ! grep -q "^$value_to_append" "$file_path"; then
-        # If not found, append the value_to_append to the file
-        echo -n "$value_to_append" >> "$file_path"
-        echo "Appended '$value_to_append' to $file_path"
+    local new_path=$1
+    # Check if the new_path is already in the $PATH
+    if [[ ":$PATH:" != *":$new_path:"* ]]; then
+        # If not, append it to the $PATH
+        export PATH="$new_path:$PATH"
+        echo "$new_path has been added to the PATH."
     else
-        echo "'$value_to_append' already exists in $file_path. Skipping append."
+        echo "$new_path is already in the PATH. Skipping."
     fi
 }
 
-# Function to configure environment variables
-function configure_env_variables() {
-    # Check if the env_export file exists and source it
-    if [ -f "$ENV_EXPORT" ]; then
-        echo -e "ENV_EXPORT: \033[0;35m$(cat "$ENV_EXPORT")\033[0m"
-        source "$ENV_EXPORT"  # Source the file to set the environment variables
-    else
-        echo "Warning: $ENV_EXPORT file not found...!"
-    fi
-
-    # Check if the ENV_PATH file exists and update PATH
-    if [ -f "$ENV_PATH" ]; then
-        echo -e "ENV_PATH: \033[0;35m$(cat "$ENV_PATH")\033[0m"
-        export PATH="$(cat "$ENV_PATH")$PATH"  # Prepend the value from ENV_PATH to the PATH variable
-    else
-        echo "Warning: $ENV_PATH file not found...!"
-    fi
-}
 function download_and_extract() {
     local lib_url=$1
     local install_dir=$2
@@ -260,7 +239,7 @@ function configure() {
     fi
 
     # Set environment variables for the library
-    generate_env_files "$lib" "$lib_version" "$install_dir"
+    configure_env_variables "$lib" "$lib_version" "$install_dir"
     
     if $run_post_installation; then
         run_installation_script "$lib" "installation.post_installation_script"
@@ -307,9 +286,6 @@ if [ $# -eq 0 ]; then
     # No argument provided, print a message
     warn "No argument provided. Example: configure jdk"
 else  
-    # Clean up environment files
-    rm -rf "$ENV_PATH" "$ENV_EXPORT"
-
     # Iterate over all the provided arguments and call configure for each
     for tool in "$@"; do
         echo "Configuring tool: $tool"
